d3d3f48669d34542f32e6754a8f918c19ce89e71
JeanLuc Robitaille <jlr@robitaille.us>
2024-09-01 16:41:01 -0400

    end of day 09/01/24

--- Assignments/Assignment 1/JDBCORACLE.java
+++ Assignments/Assignment 1/JDBCORACLE.java
@@ -73,7 +73,104 @@ public class JDBCORACLE {
          return "";
       }
    }
+   public static void menu()
+   {
+      int debitacct=-1;
+      int creditacct=-1;
+      float amount=-1;
+      boolean keepgoing=true;
+      boolean ReadyToProcess=false;
+      while(keepgoing==true)
+      {
+         System.out.println("***************************************************");
+         System.out.println("***                                             ***");
+         System.out.println("***   Hello User                                ***");
+         System.out.println("***     Welcome to JeanLuc's Simple             ***");
+         System.out.println("***         Banking JDBC Program                ***");
+         System.out.println("***                                             ***");
+         System.out.println("***                                             ***");
+         System.out.println("***  1. Enter Sending Account Number            ***");
+         System.out.println("***  2. Enter Receiving Account Number          ***");
+         System.out.println("***  3. Enter Amount to send                    ***");
+         System.out.println("***                                             ***");
+         if(creditacct!=-1&&debitacct!=-1&&amount!=-1)
+         {
+            System.out.println("***  4. Attempt Transaction                     ***");
+            ReadyToProcess=true;
+         }
+         System.out.println("***                                             ***");
+         System.out.println("***************************************************");
+         int choice = 0;
+         try
+         {
+            choice = Integer.parseInt(readEntry(" "));
+         }
+         catch (ArithmeticException e)
+         {
+            choice=0;
+         }
+         switch (choice)
+         {
+            case 1:
+               System.out.println("");
+               boolean retry=true;
+               while(retry==true)
+               {
+                  try
+                  {
+                     debitacct = Integer.parseInt(readEntry("Please enter sending account number: "));
 
-    
-    
+                  }
+                  catch (ArithmeticException e)
+                  {
+                     System.out.println("Invalid Input, ");
+                     debitacct=-1;
+                  }
+               }
+               
+            case 2:
+               System.out.println("");
+               retry=true;
+               while(retry==true)
+               {
+                  try
+                  {
+                     creditacct = Integer.parseInt(readEntry("Please enter receiving account number: "));
+
+                  }
+                  catch (ArithmeticException e)
+                  {
+                     System.out.println("Invalid input, ");
+                     creditacct=-1;
+                  }
+               }
+               
+            case 3:
+               System.out.println("");
+               retry=true;
+               while(retry==true)
+               {
+                  
+                  try
+                  {
+                     creditacct = Integer.parseInt(readEntry("Please enter amount to be transferred: "));
+
+                  }
+                  catch (ArithmeticException e)
+                  {
+                     System.out.println("Invalid input, ");
+                     creditacct=-1;
+                  }
+               }
+            case 4:
+            default:
+               System.out.println("Invalid Option, Please enter a valid option");
+
+         }
+      }
+   }
+   public static int transfer(int decacct, int incacct, float quantity)
+   {
+
+   }
 }
